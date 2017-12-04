1f8ca73cdf232e280a8f56e4a3d780624ab36464
wangabeng <wangabeng@163.com>
2017-12-04 23:28:15 +0800

    add

--- tab.js
+++ tab.js
@@ -8,7 +8,8 @@
     this.config = {
       "fade": false,
       "triggerType": "click",
-      "color": "red"
+      "color": "red",
+      'currentDefault': 0
     };
     // 重置配置参数
     if (this.config) {
@@ -22,12 +23,14 @@
     // 获取tab元素集合 及展示内容集合
     this.tabNavs = this.el.find('.tab-nav li');
     this.showCont = this.el.find('.tab-content');
+    // this.currentIndex = config.currentDefault;
 
     // 根据不同的配置参数 进行不同的事件绑定
     if (config.triggerType) {
       // 进行事件绑定
       this.tabNavs.on(config.triggerType, function () {
-        console.log($(this).index());
+        // $(this)指的是被点击的当前tab的包装对象
+        _this_.tabNav($(this), _this_.showCont); // 应用原型方法
       });
       
     }
@@ -38,9 +41,28 @@
   Tab.prototype = {
     getConfig: function(dataOld, dataNew) {
       return $.extend(dataOld, dataNew);
+    },
+    tabNav: function (cur, showCont) {
+      // cur.index() 获取当前索引值
+      var currentIndex = cur.index();
+
+      // tab的class .active切换
+      cur.addClass('active');
+      cur.siblings().removeClass('active');
+      // 设置所有展示区showCont的样式
+      showCont.eq(currentIndex).fadeIn();
+      showCont.eq(currentIndex).siblings().fadeOut();
     }
   };
 
-  window.Tab = Tab;
+  // 3 初始化 创建实例
+  function init () {
+    $('.tab-wrapper').each(function(){
+      new Tab($(this));
+    });
+  }
+
+  // window.Tab = Tab;
+  window.init = init;
 
 })();
